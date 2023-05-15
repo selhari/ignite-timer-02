8f0588499afcb355fbc551d6a4b56b554cb16b60
Matheus Santos <mathehensan@gmail.com>
2023-05-14 23:44:12 -0300

    Structure, application and lint config

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import { ButtonContainer, ButttonVariant } from './Button.styles'
+
+interface ButtonProps {
+  variant?: ButttonVariant
+}
+
+export function Button({ variant = 'primary' }: ButtonProps) {
+  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
+}
